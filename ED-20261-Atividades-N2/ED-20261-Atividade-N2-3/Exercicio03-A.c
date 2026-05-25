c008fe54cee47104d3f1f798c564936a7a0c02bb
Anna Louro <annab.louro@gmail.com>
2026-05-24 23:00:54 -0300

    Create Exercicio03-A.c

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio03-A.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void hanoi(int n,
+           char origem,
+           char destino,
+           char auxiliar) {
+
+    /* Caso base */
+    if(n == 1) {
+
+        printf("Mover disco 1 de %c para %c\n",
+               origem, destino);
+
+        return;
+    }
+
+    /* Move n-1 para auxiliar */
+    hanoi(n - 1,
+          origem,
+          auxiliar,
+          destino);
+
+    /* Move o maior disco */
+    printf("Mover disco %d de %c para %c\n",
+           n, origem, destino);
+
+    /* Move n-1 para destino */
+    hanoi(n - 1,
+          auxiliar,
+          destino,
+          origem);
+}
+
+int main() {
+
+    hanoi(3, 'A', 'C', 'B');
+
+    return 0;
+}
