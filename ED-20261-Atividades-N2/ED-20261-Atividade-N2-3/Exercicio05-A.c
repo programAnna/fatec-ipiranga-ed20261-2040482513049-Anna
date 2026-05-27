31c878f758ab94128baba34ac6258806c8265126
Anna Louro <annab.louro@gmail.com>
2026-05-26 23:27:47 -0300

    Create Exercicio05-A.c

---
+++ ED-20261-Atividades-N2/ED-20261-Atividade-N2-3/Exercicio05-A.c
@@ -0,0 +1,35 @@
+RA:  - Anna Beatriz Louro da Silva
+
+#include <stdio.h>
+
+No* inserir(No *raiz, int valor) {
+
+    // Criando novo no 
+    if(raiz == NULL) {
+
+        No *novo = malloc(sizeof(No));
+
+        novo->dado = valor;
+
+        novo->esq = NULL;
+        novo->dir = NULL;
+
+        return novo;
+    }
+
+    // esquerda
+    if(valor < raiz->dado) {
+
+        raiz->esq =
+            inserir(raiz->esq, valor);
+    }
+
+    // direita
+    else {
+
+        raiz->dir =
+            inserir(raiz->dir, valor);
+    }
+
+    return raiz;
+}
